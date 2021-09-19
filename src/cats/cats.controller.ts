f32c292a1bf2f45f1a1893815d6babf77fd1b250
kminkyung <mminkyung.k@gmail.com>
2021-09-19 22:33:51 +0900

    add controller: cats

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    console.log('id', typeof id, id);
+    return `This action returns #${id} cat`;
+  }
+}
